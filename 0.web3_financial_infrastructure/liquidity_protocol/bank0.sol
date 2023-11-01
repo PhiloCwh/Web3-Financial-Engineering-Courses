6c811e00b5ec13de84b4c672cb63786752a0b9f9
PhiloCwh <105807963+PhiloCwh@users.noreply.github.com>
2023-11-01 21:34:44 +0800

    Update bank0.sol

--- 0.web3_financial_infrastructure/liquidity_protocol/bank0.sol
+++ 0.web3_financial_infrastructure/liquidity_protocol/bank0.sol
@@ -1,36 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-
+pragma solidity ^0.8.20;
 /*
-资产存入
-资产借出
-利息计算
-资产归还
-资产清算
+存入
+取出
 */
-
+//存入erc20
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract liquidity_protocol
-{
-    /*
-    计算的过程
-    用户存入资产记住存入标签
-    用户取出资产记录取出时间（主要记录用户在不同时间的资产存量）
-    计算时用每段用户的 ∑ i>>n  debt(i) * profitrate(i) * timespan(i)
-    */
+contract bank0{
 
+    mapping (address => uint) _balance;
 
-    IERC20 asset;
+    receive() external payable { }
 
+    function depositEth() public payable  {
+        _balance[msg.sender] += msg.value;
 
-    
-
-
-    
-
-    constructor(address _asset){
-        asset = IERC20(_asset);
     }
+
+    function withdrawEth(uint _amount) public payable {
+        address payable  user = payable (msg.sender);
+        user.transfer(_amount);
+        _balance[msg.sender] -= msg.value;
+    }
+
+    
+
 }
